13bc5ec40f7e6b9b4b1e7665d010f52f40a1488b
vladyslavkundyk <113207021+vladyslavkundyk@users.noreply.github.com>
2022-09-09 22:12:46 +0300

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,9 @@
+package newpacket;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+	    System.out.println("Hello Java!");
+	        System.out.println("Hello World!");
+	}
+}
